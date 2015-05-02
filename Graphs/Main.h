7cc22b7a280fe747cfcabd90a7babd369dab33fd
Abelardo E. Mendoza <abelardo2_2@hotmail.com>
2015-05-01 23:39:45 -0600

    Made APP defines global for custom headers.

--- Graphs/Main.h
+++ Graphs/Main.h
@@ -2,11 +2,13 @@
 
 #include <Windows.h>
 
-#include "EventHandlers\MainEventHandler.h"
-#include "Infrastructure\Messages\WindowMessages.h"
-
+// Make these definitions global for the other classes
+// and headers in case we need them.
 #define APP_CLASS_NAME  TEXT("MAIN_GRAPH_WINDOW")
 #define APP_WND_TITLE   TEXT("Graphs Remake")
+
+#include "EventHandlers\MainEventHandler.h"
+#include "Infrastructure\Messages\WindowMessages.h"
 
 LRESULT CALLBACK WinProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, char *lpCmdLine, int nShowCmd);
